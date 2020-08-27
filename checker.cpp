77d4f0258826ac58dfff379f548364cbcab83cf9
shrutibnehe <56735035+shrutibnehe@users.noreply.github.com>
2020-08-27 16:29:35 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,31 +1,39 @@
 #include <assert.h>
+const int bpmrange[]={70,150};
+const int spo2range[]={90,100};
+const int resprange[]={30,95};
+
+bool checklimit(float value,int min,int max)
+{
+    return(value>=min &&value<=max);
+}
 bool bpmisok(float bpm)
 {
-    if(bpm > 70 && bpm < 150) 
-      return true;
+    if(checklimit(bpm,bpmrange[0],bpmrange[1])) 
+    return true;
 }
 bool spo2isok(float spo2)
 {
-  if(spo2>90)
-     return true;
- }
+    
+    if(checklimit(spo2,spo2range[0],spo2range[1])) 
+        return true;
+}
+
 bool respisok(float resp)
 {
-    if(resp > 30 && resp< 95) 
-       return true;
- }
+  if(checklimit(resp,resprange[0],resprange[1])) 
+        return true;
+}
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if((bpmisok(bpm)) && (spo2isok(spo2)) && (respisok(respRate)))
-  {
-      return true;
-  }
-  else
-  {
-     return false;
-  }
+  return (bpmisok(bpm) && spo2isok(spo2) && respisok(respRate));
   
-}
+ }
+
 int main() {
+  assert(checklimit(54,10,100)==true);
+  assert(checklimit(50,10,40)==false);
   assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
-}
+  assert(vitalsAreOk(80, 95, 40) == true);
+  assert(vitalsAreOk(160, 90, 40) == false);
+  assert(vitalsAreOk(90, 90, 20) == false);
+  }
