b8af2731dbb09fe993c018ec5126c3e2a6414185
shrutibnehe <56735035+shrutibnehe@users.noreply.github.com>
2020-09-02 07:43:54 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -3,10 +3,12 @@ using namespace std;
 const int bpmrange[]={90,150};
 const int spo2range[]={90,100};
 const int respRaterange[]={30,95};
+
 class Alert
 {
   public:
-     virtual void raisealert(const char*, const char*) = 0;
+
+    virtual void raisealert(const char*, const char*) = 0;
 };
 class SMSAlert: public Alert
 {
@@ -16,6 +18,7 @@ public:
      cout<<"SMS Alert  "<<vitalname<<" is "<<msg<<endl;
 	}
 };
+
 class SoundAlert:public Alert
 {
 public:
@@ -23,43 +26,39 @@ public:
 		{
 		cout<<"Sound Alert "<<vitalname<<" is "<<msg<<endl;
 		}
-        };
+
+};
 class vitals
 {
-public:
-	const char* checklimit(int vitalValue, int lowerLimit, int upperLimit)
+ public:
+	 void checklimit(const char* vitalname,int vitalValue, int lowerLimit, int upperLimit,Alert* alert)
 	{
 	      if(vitalValue < lowerLimit)
-	        return "LOW";
-	      if(vitalValue > upperLimit)
-	        return "HIGH";
-	      return "Within Range";
+	         alert->raisealert(vitalname,"LOW");
+	      else if(vitalValue > upperLimit)
+	    	  alert->raisealert(vitalname,"HIGH");
+	      else
+	        alert->raisealert(vitalname,"NORMAL");
 }
-	void vitalsAreOk(float bpm, float spo2, float respRate) {
-	      const char* bpmmsg = checklimit(bpm, bpmrange[0],bpmrange[1] );
-	      const char* spo2msg = checklimit(spo2, spo2range[0],spo2range[1] );
-	      const char* respRatemsg = checklimit(respRate, respRaterange[0],respRaterange[1] );
-
-	      Alert* alert;
-	      SMSAlert alertSMS;
-	      SoundAlert alertSound;
-
-	      alert = &alertSMS;
-	      alert->raisealert("BPM", bpmmsg);
-	      alert->raisealert("SPO2", spo2msg);
-	      alert->raisealert("Respiratory Rate", respRatemsg);
-
-	      alert = &alertSound;
-	      alert->raisealert("BPM", bpmmsg);
-	      alert->raisealert("SPO2", spo2msg);
-	      alert->raisealert("Respiratory Rate", respRatemsg);
-	    }
+	void vitalsAreOk(float bpm, float spo2, float respRate,Alert* alert) {
+	       checklimit("BPM",bpm, bpmrange[0],bpmrange[1],alert );
+	       checklimit("SPO2",spo2, spo2range[0],spo2range[1],alert );
+	       checklimit("RESPRATE",respRate, respRaterange[0],respRaterange[1],alert );
+	}
 };
 int main() {
-	vitals checkvitals;
-	  checkvitals.vitalsAreOk(95, 95, 60);
-	  checkvitals.vitalsAreOk(80, 110, 20);
-	  checkvitals.vitalsAreOk(80, 98, 45);
-	  checkvitals.vitalsAreOk(100, 80, 40);
-	  checkvitals.vitalsAreOk(90, 95, 100);
+          SMSAlert smsalert;
+	  SoundAlert soundalert;
+	  vitals checkvitals;
+	  checkvitals.vitalsAreOk(100, 95, 60, &smsalert); //No alert
+
+	  checkvitals.vitalsAreOk(155, 90, 90, &smsalert); //Bpm is high
+	  checkvitals.vitalsAreOk(140, 80, 60, &smsalert); //  spo2 is low
+	  checkvitals.vitalsAreOk(140, 95, 110, &smsalert); //resprate is high
+
+	  checkvitals.vitalsAreOk(100, 95, 60, &soundalert); //No alert
+
+	  checkvitals.vitalsAreOk(155, 90, 90, &soundalert); //Bpm is high
+	  checkvitals.vitalsAreOk(140, 80, 60, &soundalert); //  spo2 is low
+	  checkvitals.vitalsAreOk(140, 95, 110, &soundalert); //resprate is high
 }
